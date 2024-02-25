46ddaaf4b0c1fa1c89f024437e2c27e80b908fc8
omar <omarrayad74@gmail.com>
2024-02-26 00:40:29 +0200

    adding tasks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
   const [tasks, setTasks] = useState([])
+  const taskRef = useRef()
+  function addTask (){
+    const newTask = taskRef.current.value
+    setTasks([...tasks, newTask])
+    taskRef.current.value = ""
+  }
   return (
     <div className="App">
       <h2>Todo List</h2>
@@ -13,8 +19,8 @@ function App() {
           })
         }
       </ul>
-      <input placeholder="Add New Task..."></input>
-      <button>Add task</button>
+      <input ref={taskRef} placeholder="Add New Task..."></input>
+      <button onClick={addTask}>Add task</button>
     </div>
   );
 }
